3759496df99a7dec33e32d7f0f84099bcb6f6ece
SYNAX-SANATANI <166273111+SYNAX-SANATANI@users.noreply.github.com>
2024-06-29 18:53:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SYNAX-SANATANI/ProfessorMusic",
+    "https://github.com/SYNAX-SANATANI/ProfessorMusic2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
